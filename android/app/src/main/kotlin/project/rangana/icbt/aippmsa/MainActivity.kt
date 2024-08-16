d565a4c26042a6945b1c7915516862534dc47541
Rangana <rangana@asset.digital>
2024-08-16 14:07:08 +0530

    Init commit

---
+++ android/app/src/main/kotlin/project/rangana/icbt/aippmsa/MainActivity.kt
@@ -0,0 +1,5 @@
+package project.rangana.icbt.aippmsa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/rangana/project/icbt/aippmsa/MainActivity.kt
@@ -0,0 +1,5 @@
+package rangana.project.icbt.aippmsa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
